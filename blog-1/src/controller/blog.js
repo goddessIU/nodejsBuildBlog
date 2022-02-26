0bf71c042061d9f99b41f74ff6baa09d85e59fda
goddessIU <2581606394@qq.com>
2022-02-26 08:22:30 +0800

    第一次提交完成路由章节

---
+++ blog-1/src/controller/blog.js
@@ -0,0 +1,49 @@
+const getList = (author, keyword) => {
+    return [
+        {
+            id: 1,
+            title: '标题A',
+            content: '内容A',
+            createTime: 1645800527807,
+            author: 'zhangsan'
+        },
+        {
+            id: 2,
+            title: '标题B',
+            content: '内容B',
+            createTime: 202022527807,
+            author: 'lisi'
+        }
+    ]
+}
+
+const getDetail = (id) => {
+    return {
+        id: 1,
+        title: '标题A',
+        content: '内容A',
+        createTime: 1645800527807,
+        author: 'zhangsan'
+    }
+}
+
+const newBlog = (blogData = {}) => {
+    return {
+        id: 3
+    }
+}
+
+const updateBlog = (id, blogData = {}) => {
+    return true
+}
+
+const delBlog = (id) => {
+    return true
+}
+module.exports = {
+    getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog
+}
---
+++ blog-1/src/router/blog.js
@@ -0,0 +1,46 @@
+const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog.js')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+
+const handleBlogRouter = (req, res) => {
+    const method = req.method
+    const id = req.query.id
+
+    if (method === 'GET' && req.path === '/api/blog/list') {
+        const author = req.query.author || ''
+        const keyword = req.query.keyword || ''
+        const listData = getList(author, keyword)
+        return new SuccessModel(listData)
+    }
+
+    if (method === 'GET' && req.path === '/api/blog/detail') {
+        
+        const data = getDetail(id)
+        return new SuccessModel(data)
+    }
+
+    if (method === 'POST' && req.path === '/api/blog/new') {
+        const data = newBlog(req.body)
+        return new SuccessModel(data)
+    }
+
+    if (method === 'POST' && req.path === '/api/blog/update') {
+        const result = updateBlog(id, req.body)
+        if (result) {
+            return new SuccessModel()
+        } else {
+            return new ErrorModel('fail to update')
+        }
+    }
+
+    if (method === 'POST' && req.path === '/api/blog/del') {
+        const result = delBlog(id)
+        if (result) {
+            return new SuccessModel()
+        } else {
+            return new ErrorModel('fail to del')
+        }
+    }
+
+}
+
+module.exports = handleBlogRouter
